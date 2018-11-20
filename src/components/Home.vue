2b19d4acf5f93f467894df8b3099760553e53d2a
DimaSerbenyuk <d_serbenyuk@mail.ru>
2018-11-20 22:58:39 +0200

    add

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,17 @@
     section
       .container
         h1.ui-title-1 Home
+        input(
+          type="text"
+          placeholder="What we will Watch?"
+          v-model="taskTitle"
+          @keyup.enter="newTask"
+        )
+        textarea(
+        type="text"
+        v-model="taskDescription"
+          @keyup.enter="newTask"
+        )
     section
       .container
         .task-list
@@ -15,6 +26,16 @@
               .task-item__info
                 span.ui-lable.ui-lable--light {{task.whatWatch}}
                 span Total Time:
+              span.button-close
+            .task-item__content
+              .task-item__header
+                .ui-checkbox(
+                  type='checkbox'
+                  v-model="task.completed"
+                )
+              span.ui-title-3 {{task.title}}
+            .task-item__body
+              p.ui-text-regular {{task.description}}
 
 </template>
 
@@ -22,25 +43,40 @@
   export default {
     data() {
       return {
-       tasks:[
-         {
-           'id': 1,
-           'title': 'GrowthBusters: Hooked on Growth',
-           'description': 'I directed this documentary',
-           'whatWatch': 'Film',
-           'completed': 'false',
-           'editing': 'false',
-         },
-         {
-           'id': 2,
-           'title': 'Game of thrones',
-           'description': 'The winter is coming',
-           'whatWatch': 'Serial',
-           'completed': 'false',
-           'editing': 'false',
-         }
-
-       ]
+        taskTitle: '',
+        taskDescription: '',
+        whatWatch: 'Film',
+        taskId: 3,
+        tasks: [
+          {
+            'id': 1,
+            'title': 'GrowthBusters: Hooked on Growth',
+            'description': 'I directed this documentary',
+            'whatWatch': 'Film',
+            'completed': 'false',
+            'editing': 'false',
+          },
+          {
+            'id': 2,
+            'title': 'Game of thrones',
+            'description': 'The winter is coming',
+            'whatWatch': 'Serial',
+            'completed': 'false',
+            'editing': 'false',
+          }
+        ]
+      }
+    },
+    method: {
+      newTask() {
+        this.tasks.push({
+          id: this.taskId,
+          title: this.taskTitle,
+          description: this.taskDescription,
+          whatWatch: this.whatWatch,
+          completed: false,
+          editing: false
+        })
       }
     }
   }
@@ -53,6 +89,24 @@
       margin-bottom 0
   .ui-label
     margin-right 8px
+
+  .task-item__info
+    display flex
+    align-items center
+    justify-content space-between
+    margin-bottom 20px
+    .button-close
+      width 20px
+      height @width
+
+  .task-item__header
+    display flex
+    align-items center
+    margin-bottom 18px
+    .ui-checkbox-wrapper
+      margin-right 8px
+    .ui-title-3
+      margin-bottom 0
 
 
 </style>
